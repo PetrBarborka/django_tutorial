4559caeb9cac10c908d423ba255fd5d1b433d425
Petr Barborka <Petr Barborka>
2017-03-25 17:24:19 +0100

    generic views

--- polls/urls.py
+++ polls/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
 	# /polls
 	url(r'^$', views.IndexView.as_view(), name='index'),
 	# /polls/num/
-	url(r'^(?P<question_id>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 	# /polls
-	url(r'^(?P<question_id>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+	url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
 	# /polls
-	url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+	url(r'^(?P<pk>\d+)/vote/$', views.vote, name='vote'),
 ]
